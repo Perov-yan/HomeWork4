dcef31883e6275399734b3c4499dd02758f55cdc
Ян <peroff007@gmail.com>
2024-03-08 04:38:24 +0300

    Задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-
+        System.out.println("Задание 1:");
+        short age = 23;
+        if (age >= 18){
+            System.out.println("Если возраст человека равен "+ age + " то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен "+ age + " он недостиг совершеннолетия, нужно немного подождать.");
         }
+        System.out.println(" ");
+
+        System.out.println("Задание 2:");
+        short temp = 2;
+        if ( temp < 5){
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку.");
+        } else {
+            System.out.println("На улице " + temp + " можно идти без шапки.");
+        }
+        System.out.println(" ");
     }
 }
