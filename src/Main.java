aad9370fcb8c7400aaa9754fad5028546db9e011
Ян <peroff007@gmail.com>
2024-03-08 04:42:27 +0300

    Задание 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,19 @@ public class Main {
 
         System.out.println("Задание 2:");
         short temp = 2;
-        if ( temp < 5){
+        if (temp < 5){
             System.out.println("На улице " + temp + " градусов, нужно надеть шапку.");
         } else {
             System.out.println("На улице " + temp + " можно идти без шапки.");
+        }
+        System.out.println(" ");
+
+        System.out.println("Задание 3:");
+        short speed = 90;
+        if (speed > 60){
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф.");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно.");
         }
         System.out.println(" ");
     }
